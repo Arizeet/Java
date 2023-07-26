4137199d480e9bdaf82ec502e6b788b0c96acdc9
Arizeet Behera <92688073+Arizeet@users.noreply.github.com>
2023-07-26 23:56:53 +0530

    26th july

---
+++ ReverseString.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+public class ReverseString {
+    public static String reverse(String str) {
+        Stack<Character> stack = new Stack<>();
+        char[] chars = str.toCharArray();
+        for (char c : chars) {
+            stack.push(c);
+        }
+        for (int i = 0; i < str.length(); i++) {
+            chars[i] = stack.pop();
+        }
+        return new String(chars);
+    }
+    public static void main(String[] args) {
+        String str = "Arizeet";
+        String result = reverse(str);
+        System.out.println(result);
+    }
+}
