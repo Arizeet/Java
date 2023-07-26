27455b9050f4241d592c7e42dde2e2b5376082e8
Arizeet Behera <92688073+Arizeet@users.noreply.github.com>
2023-07-27 02:08:45 +0530

    27th 3rd commit

--- ValidParentheses.java
+++ ValidParentheses.java
@@ -1,3 +1,32 @@
+import java.util.Stack;
+
 public class ValidParentheses {
-    
+    static boolean isValid(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    char top = stack.peek();
+                    if ((c == ')' && top == '(') || (c == '}' && top == '{') || (c == ']' && top == '[')) {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        String str = "({})";
+        if(isValid(str)){
+            System.out.println("Valid");
+        }else{
+            System.out.println("Invalid");
+        }
+    }
 }
