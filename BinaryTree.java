b8b1af749700a1ae0aa50c5451d06097505fe523
Arizeet Behera <92688073+Arizeet@users.noreply.github.com>
2023-08-05 00:25:51 +0530

    5th August

---
+++ BinaryTree.java
@@ -0,0 +1,140 @@
+import java.util.Stack;
+
+public class BinaryTree {
+
+    private TreeNode root;
+    
+    private class TreeNode {
+        private int data;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+    void createBT() {
+        TreeNode first = new TreeNode(1);
+        TreeNode second = new TreeNode(2);
+        TreeNode third = new TreeNode(3);
+        TreeNode fourth = new TreeNode(4);
+        TreeNode fifth = new TreeNode(5);
+
+        root = first;
+        first.left = second;
+        first.right = third;
+
+        second.left = fourth;
+        second.right = fifth;
+    }
+
+    //recursive approach
+    void preOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    void inOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    void postOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    //iterative approach
+    void preOrderIterative(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            System.out.print(temp.data + " ");
+            if (temp.right != null) {
+                stack.push(temp.right);
+            }
+            if (temp.left != null) {
+                stack.push(temp.left);
+            }
+        }
+    }
+    
+    void inOrderIterative(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode temp = root;
+        while (!stack.isEmpty() || temp != null) {
+            if (temp != null) {
+                stack.push(temp);
+                temp = temp.left;
+            } else {
+                temp = stack.pop();
+                System.out.print(temp.data + " ");
+                temp = temp.right;
+            }
+
+        }
+    }
+
+    void postOrderIterative(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (!stack.isEmpty() || curr != null) {
+            if (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            } else {
+                TreeNode temp = stack.peek().right;
+                if (temp == null) {
+                    temp = stack.pop();
+                    System.out.print(temp.data+" ");
+                    while(!stack.isEmpty() && temp == stack.peek().right){
+                        temp = stack.pop();
+                        System.out.print(temp.data+" ");
+                    }
+                } else {
+                    curr = temp;
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.createBT();
+        System.out.print("Pre-order: ");
+        tree.preOrder(tree.root);
+        System.out.print("\nPre-order: ");
+        tree.preOrderIterative(tree.root);
+        System.out.print("\nIn-order: ");
+        tree.inOrder(tree.root);
+        System.out.print("\nIn-order: ");
+        tree.inOrderIterative(tree.root);
+        System.out.print("\nPost-order: ");
+        tree.postOrder(tree.root);
+        System.out.print("\nPost-order: ");
+        tree.postOrderIterative(tree.root);
+
+    }
+}
