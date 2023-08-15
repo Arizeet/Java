8e6881fa5233d1dd054278e728990cb5b1b801a6
Arizeet Behera <92688073+Arizeet@users.noreply.github.com>
2023-08-15 14:19:18 +0530

    15th Aug

--- temp.java
+++ temp.java
@@ -1,7 +1,11 @@
 public class temp {
     public static void main(String[] args) {
-        int a = -2;
-        int b = -2;
-        System.out.println(a%b);
+        int a = 100;
+        int sum = 0;
+        for (int i = 1; i <=9; i++) {
+            sum = sum + a;
+            a = a * 3;
+        }
+        System.out.println(sum);
     }
 }
